1422c755ba8e57bc14bbda1c9823a70592237d8c
Anton Tregubov <tregubov@gmail.com>
2020-06-21 03:56:35 +0300

    Init structure

---
+++ backend/bookstore/application/src/main/java/ru/faulab/cloud/bookstore/application/BookStoreApplication.java
@@ -0,0 +1,9 @@
+package ru.faulab.cloud.bookstore.application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class BookStoreApplication extends Application
+{
+}
---
+++ backend/bookstore/application/src/main/java/ru/faulab/cloud/bookstore/application/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package ru.faulab.cloud.bookstore.application;
+
+import javax.annotation.ParametersAreNonnullByDefault;
